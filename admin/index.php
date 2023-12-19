28c3a1c08edf0d418dfa62249fe8fc4ec1fd477b
Thanh Toàn <152396694+ThanhToan21TH@users.noreply.github.com>
2023-12-19 22:21:44 +0700

    Delete admin directory

--- admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();				// bắt đầu session (đặt ở đầu trang)
-header("location:ktnguoidung");	// thực hiện kiểm tra người dùng
-?>
--- admin/ktnguoidung/main.php
+++
@@ -1,9 +0,0 @@
-<?php include("../inc/top.php"); ?>
-
-<h4 class="text-info">Bảng điều khiển</h4> 
-<p>Đang cập nhật</p>
-
-
-
-
-<?php include("../inc/bottom.php"); ?>
